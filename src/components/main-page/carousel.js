5076d35f1313aa87b097ae8bb9bd13979f0df3f0
0-L-G-A <lol79lol97@gmail.com>
2021-09-23 17:12:44 +0300

    pages

---
+++ src/components/main-page/carousel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import 'mainPageStyles.css';
+
+
+function Carousel() {
+  return (
+    <div className="carousel">
+      
+    </div>
+  );
+}
+
+export default Carousel;
--- src/components/pages/mainPage.js
+++ src/components/pages/mainPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import '../main-page//mainPageStyles.css';
+import '../main-page/mainPageStyles.css'; 
+import Carousel from '../main-page/carousel';
 
 
 function MainPage() {
   return (
     <div className="page">
-      
+      <Carousel/>
     </div>
   );
 }
