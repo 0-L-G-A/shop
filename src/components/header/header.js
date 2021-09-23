2b371f241f46b267d4237028903d35f91c1909fd
filiczini <filiczini@gmail.com>
2021-09-23 23:12:28 +0200

    some styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,16 @@
-import React from "react";
-import HeaderMenu from "./menu";
-import HeaderLogo from "./logo";
+import React from 'react';
+import HeaderMenu from './menu';
+import HeaderLogo from './logo';
 
 function Header(headerData) {
   return (
-    <div className="header">
+    <div className="header container flex-row">
+      <HeaderMenu />
       <HeaderLogo
-        url={"https://placekitten.com/g/64/64"}
-        text={"Hello Kitty"}
+        url={
+          'https://static.vans.eu/ui/www/images/logos/logo.hash-6a49946697efd93990ed1c389cad00d5.svg'
+        }
+        text={'Hello Kitty'}
       />
       <HeaderMenu />
     </div>
