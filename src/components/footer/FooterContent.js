7c3d909a82103cdb4c41bfd42e6310563e9c4f3b
filiczini <filiczini@gmail.com>
2021-09-25 17:58:00 +0200

    menu done

--- src/components/footer/FooterContent.js
+++ src/components/footer/FooterContent.js
@@ -3,7 +3,7 @@ import Logo from '../common-comp/Logo';
 import Menu from '../menu/Menu';
 import TextContent from '../common-comp/content';
 
-function FooterContent({ title, text, Menu }) {
+function FooterContent({ title, text, menuData }) {
   return (
     <div className="FooterContent flex-row">
       <TextContent title={title} text={text} />
@@ -12,8 +12,9 @@ function FooterContent({ title, text, Menu }) {
           'https://static.vans.eu/ui/www/images/logos/logo.hash-6a49946697efd93990ed1c389cad00d5.svg'
         }
         text={'Hello Vans'}
+        height="50px"
       />
-      {/* <Menu /> */}
+      <Menu data={menuData} />
     </div>
   );
 }
