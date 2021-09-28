94e27301d2bea423d304361482c898de9d813f86
0-L-G-A <lol79lol97@gmail.com>
2021-09-28 21:46:24 +0300

    functionality for filters

---
+++ src/data/items.js
@@ -0,0 +1,58 @@
+export const items = [
+    {
+      nameItem: 'Shoes',
+      imgSrc:
+        'https://cdn.shopify.com/s/files/1/0419/1525/files/2800x1080-Women-Combat-Black-1_800x533_crop_right.jpg?v=1603303506',
+      imgAlt: 'boots',
+      contentTitle: 'Winter boots',
+      contentText: 'comfortable, waterproof winter boots',
+      currency: '$',
+      price: '150',
+      clsNameBtn: 'black-btn btn',
+      valBtn: 'View more',
+      color: "black",
+      season: 'demi-season'
+    },
+    {
+      nameItem: 'Shoes',
+      imgSrc:
+        'https://www.supershoes.com/common/images/categories/Womens-Dress-header-mobile.jpg',
+      imgAlt: 'boots',
+      contentTitle: 'Shoes for women',
+      contentText: 'uncomfortable, cheap, high-heeled shoes',
+      currency: '$',
+      price: '30',
+      clsNameBtn: 'black-btn btn',
+      valBtn: 'View more',
+      color: "black",
+      season: 'summer'
+    },
+    {
+      nameItem: 'Shoes',
+      imgSrc:
+        'https://images.ua.prom.st/2873552968_w640_h640_2873552968.jpg',
+      imgAlt: 'boots',
+      contentTitle: 'Sneakers',
+      contentText: 'the most comgortable sneakers in the world',
+      currency: '$',
+      price: '100',
+      clsNameBtn: 'black-btn btn',
+      valBtn: 'View more',
+      color: "multicolor",
+      season: 'demi-season'
+    },
+    {
+      nameItem: 'Shoes',
+      imgSrc:
+        'https://www.gannett-cdn.com/presto/2020/11/27/USAT/0cafc0d7-0c4c-4be6-b83e-bd62229f04a2-21.jpg?width=660&height=372&fit=crop&format=pjpg&auto=webp',
+      imgAlt: 'boots',
+      contentTitle: 'Winter boots',
+      contentText: 'cheap, warm, comfortable autumn boots',
+      currency: '$',
+      price: '80',
+      clsNameBtn: 'black-btn btn',
+      valBtn: 'View more',
+      color: "grey",
+      season: 'winter'
+    },
+];
